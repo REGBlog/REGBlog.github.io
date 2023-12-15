e563200ed2f67128f372dde343807b1c95984839
rgerjeki <reesegerjekian@icloud.com>
2023-12-15 18:58:15 -0500

    test chrome

--- static/js/app.js
+++ static/js/app.js
@@ -10,28 +10,28 @@ toggle.addEventListener("click", ()=>{
 })
 
 let deferredPrompt;
+
 window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the mini-infobar from appearing on mobile
   e.preventDefault();
-  // Stash the event so it can be triggered later.
   deferredPrompt = e;
-  // Update UI notify the user they can add to home screen
+  // Show the button
   document.getElementById('addToHomeScreen').style.display = 'block';
 });
 
 document.getElementById('addToHomeScreen').addEventListener('click', () => {
-  // Hide our user interface that shows our A2HS button
-  document.getElementById('addToHomeScreen').style.display = 'none';
-  // Show the prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice.then((choiceResult) => {
-    if (choiceResult.outcome === 'accepted') {
-      console.log('User accepted the A2HS prompt');
-    } else {
-      console.log('User dismissed the A2HS prompt');
-    }
-    deferredPrompt = null;
-  });
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+      document.getElementById('addToHomeScreen').style.display = 'none';
+    }).catch((error) => {
+      console.error("A2HS prompt error:", error);
+    });
+  }
 });
 
