5cc8c25475910962fd84f77d2700bd9483c6121c
rgerjeki <reesegerjekian@icloud.com>
2023-12-15 18:11:19 -0500

    addtohomescreen

--- public/sw.js
+++ public/sw.js
@@ -36,4 +36,15 @@ precacheAndRoute([
     { url: '/404.html', revision: '383676' },
 ]);
 
+self.addEventListener('beforeinstallprompt', event => { 
+    event.preventDefault(); 
+    const installButton = document.getElementById('install-button'); 
+    if (installButton) { 
+      installButton.style.display = 'block'; 
+      installButton.addEventListener('click', () => { 
+        event.prompt(); 
+      }); 
+    } 
+  }); 
+
 
