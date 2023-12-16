b57a44aa7531335797ec3e47414f6b748b2739e4
rgerjeki <reesegerjekian@icloud.com>
2023-12-15 19:03:27 -0500

    revert

--- public/js/app.js
+++ public/js/app.js
@@ -9,29 +9,3 @@ toggle.addEventListener("click", ()=>{
   body.classList.toggle("light")
 })
 
-let deferredPrompt;
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the mini-infobar from appearing on mobile
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can add to home screen
-  document.getElementById('addToHomeScreen').style.display = 'block';
-});
-
-document.getElementById('addToHomeScreen').addEventListener('click', () => {
-  // Hide our user interface that shows our A2HS button
-  document.getElementById('addToHomeScreen').style.display = 'none';
-  // Show the prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice.then((choiceResult) => {
-    if (choiceResult.outcome === 'accepted') {
-      console.log('User accepted the A2HS prompt');
-    } else {
-      console.log('User dismissed the A2HS prompt');
-    }
-    deferredPrompt = null;
-  });
-});
-
