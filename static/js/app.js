0c30c12b3c305816f408e239952e6c09ca3ad68a
rgerjeki <reesegerjekian@icloud.com>
2023-12-15 19:03:11 -0500

    revert

--- static/js/app.js
+++ static/js/app.js
@@ -9,29 +9,3 @@ toggle.addEventListener("click", ()=>{
   body.classList.toggle("light")
 })
 
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  e.preventDefault();
-  deferredPrompt = e;
-  // Show the button
-  document.getElementById('addToHomeScreen').style.display = 'block';
-});
-
-document.getElementById('addToHomeScreen').addEventListener('click', () => {
-  if (deferredPrompt) {
-    deferredPrompt.prompt();
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
-      }
-      deferredPrompt = null;
-      document.getElementById('addToHomeScreen').style.display = 'none';
-    }).catch((error) => {
-      console.error("A2HS prompt error:", error);
-    });
-  }
-});
-
