027b0f7cae169489706220546a7f6328fd3881d0
rgerjeki <reesegerjekian@icloud.com>
2023-12-18 22:39:43 -0500

    update search bar

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,24 @@
-const mode = localStorage.getItem("mode") || "";
-const toggle = document.querySelector(".toggle");
-const body = document.querySelector("body");
+const toggle = document.querySelector('.toggle');
+const body = document.body;
 
-document.body.className = mode;
+// Retrieve the saved mode from local storage or default to an empty string
+const savedMode = localStorage.getItem('mode') || '';
 
-toggle.addEventListener("click", ()=>{
-  localStorage.setItem("mode", mode === "light" ? "" : "light")
-  body.classList.toggle("light")
-})
+// Apply the saved mode (if any) to the body
+body.className = savedMode;
+
+toggle.addEventListener('click', () => {
+  // Check if the current mode is light
+  const isLightMode = body.classList.contains('light');
+
+  if (isLightMode) {
+    // If it's light mode, remove the light class and update local storage
+    body.classList.remove('light');
+    localStorage.setItem('mode', '');
+  } else {
+    // If it's not light mode, add the light class and update local storage
+    body.classList.add('light');
+    localStorage.setItem('mode', 'light');
+  }
+});
 
